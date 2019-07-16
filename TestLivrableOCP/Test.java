07cae06c2242c342ff4695083a40f69c1a9fe0db
Samir Romdhani <romdhanisamir94@gmail.com>
2019-07-17 01:30:47 +0200

    lambda expr test : Binary Versions & UnaryOperator

--- TestLivrableOCP/Test.java
+++ TestLivrableOCP/Test.java
@@ -1,23 +1,35 @@
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.IntPredicate;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 /**
  * @author samirromdhani
  */
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("############  Primitive Versions of Predicate Interface ################");
-		IntPredicate evenNums = i -> (i % 2) == 0;
-		IntStream.range(1, 10).filter(evenNums).forEach(System.out::println);
-		System.out.println("############  Primitive Versions of Function Interface ################");
-		AtomicInteger ints = new AtomicInteger(0);
-		/* Or IntStream */Stream.generate(ints::incrementAndGet).limit(10).forEach(System.out::println);
-		System.out.println("############  Primitive Versions of Consumer Interface ################");
-		// IntConsumer -> void accept(int value)
-		// ObjIntConsumer<T> -> void accept(T t, int value)
-		System.out.println("############  Primitive Versions of Supplier Interface ################");
-		// IntSupplier -> boolean getAsInt()
+		System.out.println("############  Binary Versions of Functional Interfaces ################");
+		BiFunction<String, String, String> concatStr = (x, y) -> x + y;
+		System.out.println(concatStr.apply("hello ", "world"));
+		
+		BiFunction<Double, Double, Integer> compareDoubles = Double::compare;
+		System.out.println(compareDoubles.apply(10.0, 10.0));
+		
+		BiPredicate<List<Integer>, Integer> listContains = List::contains;
+		List aList = Arrays.asList(10, 20, 30);
+		System.out.println(listContains.test(aList, 20));
+		
+		BiConsumer<List<Integer>, Integer> listAddElement = List::add;
+		List aList2 = new ArrayList();
+		listAddElement.accept(aList2, 10);
+		System.out.println(aList2);
+		
+		System.out.println("############  The UnaryOperator Interface ################");
+		List<Integer> ell = Arrays.asList(-11, 22, 33, -44, 55);
+		System.out.println("Before: " + ell);
+		ell.replaceAll(Math::abs);
+		System.out.println("After: " + ell);
 	}
 }
