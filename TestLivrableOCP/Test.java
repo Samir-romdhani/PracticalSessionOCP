ab4462a7c6dceb18a7dec223c551bd07c26338fe
Samir Romdhani <romdhanisamir94@gmail.com>
2019-07-16 02:27:52 +0200

    test 1

--- TestLivrableOCP/Test.java
+++ TestLivrableOCP/Test.java
@@ -1,9 +1,18 @@
+import java.util.function.Predicate;
+
 /**
  * @author samirromdhani
  */
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Predicate<String> nullCheck = arg -> arg != null;
+		System.out.println("test : "+nullCheck);
+		Predicate<String> emptyCheck = arg -> arg.length() > 0;
+		Predicate<String> nullAndEmptyCheck = nullCheck.and(emptyCheck);
+		String helloStr = "hello";
+		System.out.println(nullAndEmptyCheck.test(helloStr));
+		String nullStr = null;
+		System.out.println(nullAndEmptyCheck.test(nullStr));
 	}
 }
