af740d30a9a979453b51147e44d7ac9762298f9b
Samir Romdhani <romdhanisamir94@gmail.com>
2019-07-26 00:23:58 +0200

    thread ..

--- TestLivrableOCP/Test.java
+++ TestLivrableOCP/Test.java
@@ -1,9 +1,18 @@
+import java.util.Optional;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
 /**
  * @author samirromdhani
  */
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		MyRunnable r = new MyRunnable();
+		Thread foo = new Thread(r);
+		foo.start();
+		Thread bar = new Thread(r);
+		bar.start();
+		Thread bat = new Thread(r);
 	}
 }
